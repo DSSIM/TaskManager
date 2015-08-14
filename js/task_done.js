dd5aae90369e755596436eb40d164fd8671405b9
pinchukd <pinchukd@ua.fm>
2015-08-14 14:02:41 +0300

    added simple events

--- js/task_done.js
+++ js/task_done.js
@@ -1,9 +1,30 @@
-function doneTask(){
+var listToDoId="ul1";
+var listDoneId="ul2";
+
+function doneTask(event){
 //task done
+	var taskLi=event.target;
+	while(taskLi != event.currentTarget & taskLi.nodeName != "LI"){
+		taskLi=taskLi.parentElement;
+	}
+	if(taskLi.)
 
 }
 
-function undoneTask(){
+function undoneTask(event){
 //task undone
-
+	if(event.target.parentElement.id == listDoneId) {
+		console.dir(event);	
+	}
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+	//alert("started");
+	document.getElementById(listToDoId).addEventListener("change",doneTask);
+	document.getElementById(listDoneId).addEventListener("change",undoneTask);
+});
+
+
+//alert("started");
+
+
