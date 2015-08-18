5013eebfdca326ec64f733e3c549745629e844ac
igor0ser <igor0ser@gmail.com>
2015-08-18 16:06:56 +0300

    added login/register toggle

--- main-igor.js
+++ main-igor.js
@@ -1,11 +1,8 @@
-(function(){
 		regClassList = document.getElementById("reg-form").classList;
 		 moveUp = function(e){
 			if (e.target.getAttribute("data-action")){
 				regClassList.toggle('up');
-				alert(regClassList);
 			}
 		};
 
 	document.addEventListener('click', moveUp);
-})();
