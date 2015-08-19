954eec5d23d961b40cb6bd308d429b2483f801cc
Igor <igor0ser@gmail.com>
2015-08-19 10:59:36 +0300

    i tried to fix placeholder

--- main-igor.js
+++ main-igor.js
@@ -1,8 +1,17 @@
-		regClassList = document.getElementById("reg-form").classList;
-		 moveUp = function(e){
-			if (e.target.getAttribute("data-action")){
-				regClassList.toggle('up');
-			}
-		};
+var regClassList = document.getElementById("reg-form").classList;
 
-	document.addEventListener('click', moveUp);
+function moveRegFormUp(e){
+		if (e.target.getAttribute("data-action")){
+			regClassList.toggle('up');
+		}
+	};
+
+function deletePlaceholder(e){
+	if (e.target.getAttribute('placeholder')){
+		e.target.setAttribute('placeholder', '');
+	}
+}
+
+
+document.addEventListener('click', moveRegFormUp);
+//document.addEventListener('focus', deletePlaceholder);	
